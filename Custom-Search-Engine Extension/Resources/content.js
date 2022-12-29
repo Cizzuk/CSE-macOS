fd947159074c54f0dd1eb5baa12020f32a5404a2
tsg0o0 <tsg0o0@tsg0o0.com>
2022-12-29 22:12:46 +0900

    Fixed a redirect loop in DuckDuckGo

--- Custom-Search-Engine Extension/Resources/content.js
+++ Custom-Search-Engine Extension/Resources/content.js
@@ -1,19 +1,35 @@
 
 var URLtop = "";
 var URLsuffix = "";
+var avoidRedirectLoop = "1";
 
 browser.runtime.sendMessage({
     type: "content"
 },
-    function(response) {
-        URLtop = response.top;
-        URLsuffix = response.suffix;
-    if (getParam('q') != null) {
-        location.href = "https://" + URLtop + getParam('q') + URLsuffix;
-        document.getElementsByTagName('html')[0].innerHTML = '<body style="background-color:#000">';
-        console.log("CSE: URL has been rewritten.")
+function(response) {
+    URLtop = response.top;
+    URLsuffix = response.suffix;
+    URLsuffix = response.suffix;
+    avoidRedirectLoop = response.avoidRL;
+    console.log(avoidRedirectLoop)
+    if (avoidRedirectLoop == "0") {
+        if (getParam('q') != null) {
+            doRedirect();
+            console.log("a")
+        }
+    }else{
+        if ((getParam('q') != null) && (getParam('t') == "osx")) {
+            doRedirect();
+            console.log("b")
+        }
     }
 });
+
+function doRedirect() {
+    location.href = "https://" + URLtop + getParam('q') + URLsuffix;
+    document.getElementsByTagName('html')[0].innerHTML = '<body style="background:#000;Color:#ccc;font-family:sans-serif;text-align:center"><h1>Customize Search Engine</h1><p><strong>Redirecting...</strong></p></body>';
+    console.log("CSE: URL has been rewritten.")
+}
 
 function getParam(name, url) {
     if (!url) url = window.location.href;
--- Custom-Search-Engine/ViewController.swift
+++ Custom-Search-Engine/ViewController.swift
@@ -11,12 +11,22 @@ class ViewController: NSViewController, NSTextFieldDelegate{
     
     @IBOutlet weak var topText: NSTextField!
     @IBOutlet weak var suffixText: NSTextField!
+    @IBOutlet weak var avoidRLSwitch: NSSwitch!
+    var avoidRL = "1"
     
     let userDefaults = UserDefaults(suiteName: "group.com.tsg0o0.Custom-Search-Engine")
     override func viewDidLoad() {
         super.viewDidLoad()
         topText.stringValue = userDefaults!.string(forKey: "urltop") ?? "twitter.com/search?q="
         suffixText.stringValue = userDefaults!.string(forKey: "urlsuffix") ?? "&f=live"
+        avoidRL = userDefaults!.string(forKey: "avoidRL") ?? "1"
+        if avoidRL == "0" {
+            avoidRLSwitch.state = NSControl.StateValue.off
+//            avoidRLSwitch.sendAction(for: .valueChanged)
+        }else{
+            avoidRLSwitch.state = NSControl.StateValue.on
+//            avoidRLSwitch.sendActions(for: .valueChanged)
+        }
         topText.delegate = self
         suffixText.delegate = self
     }
@@ -36,5 +46,14 @@ class ViewController: NSViewController, NSTextFieldDelegate{
     @IBAction func editInputSuffix(_ sender: Any) {
         userDefaults!.set(suffixText.stringValue, forKey: "urlsuffix")
     }
-
+    @IBAction func avoidRLChange(_ sender: Any) {
+        if avoidRLSwitch.state == NSControl.StateValue.on {
+            avoidRL = "1"
+            userDefaults!.set(avoidRL, forKey: "avoidRL")
+        }else{
+            avoidRL = "0"
+            userDefaults!.set(avoidRL, forKey: "avoidRL")
+        }
+    }
+    
 }
